924dae99dd5fdf702f7b22879157334d59155908
Blake Peterson <blake.ptrsn18@gmail.com>
2023-10-06 12:45:25 -0600

    Implement rook piece moves and pass those tests

--- src/main/chessImplementation/ChessPieceImplementation.java
+++ src/main/chessImplementation/ChessPieceImplementation.java
@@ -75,6 +75,46 @@ public class ChessPieceImplementation implements ChessPiece {
     }
 
     public Collection<ChessMove> cardinalMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPosition currentPosition = myPosition;
+
+        while (currentPosition.getRow() + 1 <= 8 && (board.getPiece(new ChessPositionImplementation(currentPosition.getRow() + 1, currentPosition.getColumn())) == null || board.getPiece(new ChessPositionImplementation(currentPosition.getRow() + 1, currentPosition.getColumn())).getTeamColor() != this.color)) {
+            if (board.getPiece(new ChessPositionImplementation(currentPosition.getRow() + 1, currentPosition.getColumn())) != null && board.getPiece(new ChessPositionImplementation(currentPosition.getRow() + 1, currentPosition.getColumn())).getTeamColor() != this.getTeamColor()) {
+                moves.add(new ChessMoveImplementation(myPosition, new ChessPositionImplementation(currentPosition.getRow() + 1, currentPosition.getColumn()), null));
+                break;
+            }
+            moves.add(new ChessMoveImplementation(myPosition, new ChessPositionImplementation(currentPosition.getRow() + 1, currentPosition.getColumn()), null));
+            currentPosition = new ChessPositionImplementation(currentPosition.getRow() + 1, currentPosition.getColumn()); // move up one
+        }
+
+        currentPosition = myPosition;
+        while (currentPosition.getColumn() - 1 >= 1 && (board.getPiece(new ChessPositionImplementation(currentPosition.getRow(), currentPosition.getColumn() - 1)) == null || board.getPiece(new ChessPositionImplementation(currentPosition.getRow(), currentPosition.getColumn() - 1)).getTeamColor() != this.color)) {
+            if (board.getPiece(new ChessPositionImplementation(currentPosition.getRow(), currentPosition.getColumn() - 1)) != null && board.getPiece(new ChessPositionImplementation(currentPosition.getRow(), currentPosition.getColumn() - 1)).getTeamColor() != this.getTeamColor()) {
+                moves.add(new ChessMoveImplementation(myPosition, new ChessPositionImplementation(currentPosition.getRow(), currentPosition.getColumn() - 1), null));
+                break;
+            }
+            moves.add(new ChessMoveImplementation(myPosition, new ChessPositionImplementation(currentPosition.getRow(), currentPosition.getColumn() - 1), null));
+            currentPosition = new ChessPositionImplementation(currentPosition.getRow(), currentPosition.getColumn() - 1); // move left one
+        }
+
+        currentPosition = myPosition;
+        while (currentPosition.getRow() - 1 >= 1 && (board.getPiece(new ChessPositionImplementation(currentPosition.getRow() - 1, currentPosition.getColumn())) == null || board.getPiece(new ChessPositionImplementation(currentPosition.getRow() - 1, currentPosition.getColumn())).getTeamColor() != this.color)) {
+            if (board.getPiece(new ChessPositionImplementation(currentPosition.getRow() - 1, currentPosition.getColumn())) != null && board.getPiece(new ChessPositionImplementation(currentPosition.getRow() - 1, currentPosition.getColumn())).getTeamColor() != this.getTeamColor()) {
+                moves.add(new ChessMoveImplementation(myPosition, new ChessPositionImplementation(currentPosition.getRow() - 1, currentPosition.getColumn()), null));
+                break;
+            }
+            moves.add(new ChessMoveImplementation(myPosition, new ChessPositionImplementation(currentPosition.getRow() - 1, currentPosition.getColumn()), null));
+            currentPosition = new ChessPositionImplementation(currentPosition.getRow() - 1, currentPosition.getColumn()); // move down one
+        }
+
+        currentPosition = myPosition;
+        while (currentPosition.getColumn() + 1 <= 8 && (board.getPiece(new ChessPositionImplementation(currentPosition.getRow(), currentPosition.getColumn() + 1)) == null || board.getPiece(new ChessPositionImplementation(currentPosition.getRow(), currentPosition.getColumn() + 1)).getTeamColor() != this.color)) {
+            if (board.getPiece(new ChessPositionImplementation(currentPosition.getRow(), currentPosition.getColumn() + 1)) != null && board.getPiece(new ChessPositionImplementation(currentPosition.getRow(), currentPosition.getColumn() + 1)).getTeamColor() != this.getTeamColor()) {
+                moves.add(new ChessMoveImplementation(myPosition, new ChessPositionImplementation(currentPosition.getRow(), currentPosition.getColumn() + 1), null));
+                break;
+            }
+            moves.add(new ChessMoveImplementation(myPosition, new ChessPositionImplementation(currentPosition.getRow(), currentPosition.getColumn() + 1), null));
+            currentPosition = new ChessPositionImplementation(currentPosition.getRow(), currentPosition.getColumn() + 1); // move right one
+        }
         return moves;
     }
 }
--- src/main/chessImplementation/Rook.java
+++ src/main/chessImplementation/Rook.java
@@ -1,9 +1,18 @@
 package chessImplementation;
 
+import chess.ChessBoard;
 import chess.ChessGame;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.Collection;
 
 public class Rook extends ChessPieceImplementation {
     public Rook(ChessGame.TeamColor color, PieceType type) {
         super(color, type);
+    }
+
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        return cardinalMoves(board, myPosition);
     }
 }
