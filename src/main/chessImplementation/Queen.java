7bd2558ade713d909e26e023138a3c1a07df1f32
Blake Peterson <blake.ptrsn18@gmail.com>
2023-10-06 12:48:49 -0600

    Implement queen piece moves and get those tests to pass

--- src/main/chessImplementation/Queen.java
+++ src/main/chessImplementation/Queen.java
@@ -1,9 +1,23 @@
 package chessImplementation;
 
+import chess.ChessBoard;
 import chess.ChessGame;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Queen extends ChessPieceImplementation {
     public Queen(ChessGame.TeamColor color, PieceType type) {
         super(color, type);
+    }
+
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        Set<ChessMove> allMoves = new HashSet<>();
+        allMoves.addAll(diagonalMoves(board, myPosition));
+        allMoves.addAll(cardinalMoves(board, myPosition));
+        return allMoves;
     }
 }
