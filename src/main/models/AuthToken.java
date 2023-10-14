bb3a5b8b63a3e5861b86b4248c459034d7569709
Blake Peterson <blake.ptrsn18@gmail.com>
2023-10-14 17:17:13 -0600

    Add base object structure for ClearResponse and add data fields and methods to AuthToken and Game as well

--- src/main/models/AuthToken.java
+++ src/main/models/AuthToken.java
@@ -1,4 +1,35 @@
 package models;
 
+/**
+ * Object representation of an auth token used to authenticate web API calls after a user has logged in
+ */
 public class AuthToken {
+    private final String username;
+    private final String authToken;
+
+    /**
+     * AuthToken object constructor that takes in a username and auth token to instantiate
+     * @param username The username of the user the auth token is associated with
+     * @param authToken The auth token the user can use to make web API calls
+     */
+    public AuthToken(String username, String authToken) {
+        this.username = username;
+        this.authToken = authToken;
+    }
+
+    /**
+     * A getter method for user's username
+     * @return The associated user's username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * A getter method for the auth token
+     * @return The auth token
+     */
+    public String getAuthToken() {
+        return authToken;
+    }
 }
--- src/main/models/Game.java
+++ src/main/models/Game.java
@@ -2,6 +2,9 @@ package models;
 
 import chess.ChessGame;
 
+/**
+ * Object representation of a game with an id that can be used as a room to host online chess games
+ */
 public class Game {
     private int gameID;
     private String whiteUsername;
@@ -9,6 +12,16 @@ public class Game {
     private String gameName;
     private ChessGame game;
 
+    /**
+     * Game object constructor that will have a game ID and the usernames of the players playing, the name of the game,
+     * and the object representing the chess game with its board itself
+     *
+     * @param gameID
+     * @param whiteUsername
+     * @param blackUsername
+     * @param gameName
+     * @param game
+     */
     public Game(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
         this.gameID = gameID;
         this.whiteUsername = whiteUsername;
@@ -17,42 +30,82 @@ public class Game {
         this.game = game;
     }
 
+    /**
+     * A getter method for the game's ID
+     * @return The game's ID
+     */
     public int getGameID() {
         return gameID;
     }
 
+    /**
+     * A setter method for setting the game's ID
+     * @param gameID The game's new game ID
+     */
     public void setGameID(int gameID) {
         this.gameID = gameID;
     }
 
+    /**
+     * A getter method for the username of the game's white team player
+     * @return The game's white team player name
+     */
     public String getWhiteUsername() {
         return whiteUsername;
     }
 
+    /**
+     * A setter method for setting the username of the game's white team player
+     * @param whiteUsername The new username for the game's white team player
+     */
     public void setWhiteUsername(String whiteUsername) {
         this.whiteUsername = whiteUsername;
     }
 
+    /**
+     * A getter method for the username of the game's black team
+     * @return The game's black team player name
+     */
     public String getBlackUsername() {
         return blackUsername;
     }
 
+    /**
+     * A setter method for setting the username of the game's black team player
+     * @param blackUsername The new username for the game's black team player
+     */
     public void setBlackUsername(String blackUsername) {
         this.blackUsername = blackUsername;
     }
 
+    /**
+     * A getter method for the game's name
+     * @return The game's name
+     */
     public String getGameName() {
         return gameName;
     }
 
+    /**
+     * A setter method for setting the game's name
+     * @param gameName The game's new name
+     */
     public void setGameName(String gameName) {
         this.gameName = gameName;
     }
 
+    /**
+     * A getter method for the game's chess game object that includes the board, rules, and pieces
+     * @return The game's chess game object
+     */
     public ChessGame getGame() {
         return game;
     }
 
+    /**
+     * A setter method for setting the game's chess game object
+     * @param game The game's new chess game object
+     */
     public void setGame(ChessGame game) {
         this.game = game;
     }
